989912c8ddf12e05fb3f976c3436c7cc7b2ba6ab
Cristian Magherusan-Stanciu <cristi.magherusan@gmail.com>
2026-02-28 07:24:17 +0100

    Add state machine unit tests
    - Test transition calls exit on previous state and enter on target
    - Test message passing through transitions
    - Test transition to invalid state name doesn't crash

---
+++ tests/test_state_machine.gd
@@ -0,0 +1,52 @@
+extends GutTest
+## Tests for the StateMachine and State base classes.
+
+
+class MockState:
+	extends State
+	var entered := false
+	var exited := false
+	var last_msg: Dictionary = {}
+
+	func enter(msg: Dictionary = {}) -> void:
+		entered = true
+		last_msg = msg
+
+	func exit() -> void:
+		exited = true
+
+
+func test_transition_calls_exit_and_enter() -> void:
+	var sm := StateMachine.new()
+	var state_a := MockState.new()
+	state_a.name = "StateA"
+	var state_b := MockState.new()
+	state_b.name = "StateB"
+	sm.add_child(state_a)
+	sm.add_child(state_b)
+	sm.initial_state = state_a
+	add_child_autofree(sm)
+
+	# Wait one frame for _ready
+	await get_tree().process_frame
+
+	assert_true(state_a.entered, "Initial state should be entered")
+
+	sm.transition_to("StateB", {"reason": "test"})
+	assert_true(state_a.exited, "Previous state should be exited")
+	assert_true(state_b.entered, "Target state should be entered")
+	assert_eq(state_b.last_msg, {"reason": "test"}, "Message should be passed")
+
+
+func test_transition_to_invalid_state_does_not_crash() -> void:
+	var sm := StateMachine.new()
+	var state_a := MockState.new()
+	state_a.name = "StateA"
+	sm.add_child(state_a)
+	sm.initial_state = state_a
+	add_child_autofree(sm)
+	await get_tree().process_frame
+
+	# Should log error but not crash
+	sm.transition_to("NonExistent")
+	assert_eq(sm.current_state, state_a, "Should remain in current state")
