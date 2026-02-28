74591c9d8853c7c3e006b57fb4dd965879a98906
Cristian Magherusan-Stanciu <cristi.magherusan@gmail.com>
2026-02-28 08:43:40 +0100

    Add player scene with CharacterBody3D and state machine
    - Capsule collider on PlayerFoot layer, masks Ground+Static
    - InteractionArea monitors Trigger layer for vehicle proximity
    - StateMachine with 6 states, PlayerCamera, PlayerUI children
    - Activates player camera and FOOT context on ready

---
+++ scenes/player/player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody3D
+## Player character controller. Owns state machine, interaction detection, and camera.
+
+@export var walk_speed := 4.0
+@export var run_speed := 8.0
+@export var gravity := 20.0
+@export var rotation_speed := 10.0
+
+var nearest_vehicle: Node = null
+var current_vehicle: Node = null
+
+@onready var player_camera: Node3D = $PlayerCamera
+@onready var mesh: MeshInstance3D = $Mesh
+
+
+func _ready() -> void:
+	InputManager.set_context(InputManager.Context.FOOT)
+	player_camera.make_active()
+	$InteractionArea.area_entered.connect(_on_interaction_area_entered)
+	$InteractionArea.area_exited.connect(_on_interaction_area_exited)
+
+
+func _on_interaction_area_entered(area: Area3D) -> void:
+	if area.is_in_group("vehicle_interaction"):
+		nearest_vehicle = area.get_parent()
+		var sm := $StateMachine
+		var foot_states := ["idle", "walking", "running"]
+		if sm.current_state and sm.current_state.name.to_lower() in foot_states:
+			EventBus.show_interaction_prompt.emit("Hold F to steal")
+
+
+func _on_interaction_area_exited(area: Area3D) -> void:
+	if area.is_in_group("vehicle_interaction"):
+		if nearest_vehicle == area.get_parent():
+			nearest_vehicle = null
+			EventBus.hide_interaction_prompt.emit()
