18790013ea75602a091d6d4c78a07223e6720bc1
Cristian Magherusan-Stanciu <cristi.magherusan@gmail.com>
2026-02-28 07:38:53 +0100

    Fix camera not following vehicle
    - @export var target: Node3D with NodePath in .tscn doesn't auto-resolve
    - Changed to @export var target_path: NodePath and resolve via get_node() in _ready()
    - Renamed internal reference to _target to satisfy gdlint ordering

--- scenes/vehicles/vehicle_camera.gd
+++ scenes/vehicles/vehicle_camera.gd
@@ -1,7 +1,7 @@
 extends Node3D
 ## Smooth chase camera for vehicles with speed-based distance and look-ahead.
 
-@export var target: Node3D
+@export var target_path: NodePath
 @export var min_distance := 5.0
 @export var max_distance := 8.0
 @export var min_height := 2.0
@@ -12,6 +12,7 @@ extends Node3D
 @export var speed_for_max_distance := 150.0
 
 var _current_velocity := Vector3.ZERO
+var _target: Node3D
 
 @onready var spring_arm: SpringArm3D = $SpringArm3D
 @onready var camera: Camera3D = $SpringArm3D/Camera3D
@@ -19,17 +20,19 @@ var _current_velocity := Vector3.ZERO
 
 func _ready() -> void:
 	set_as_top_level(true)
-	if target:
-		global_position = target.global_position
+	if target_path:
+		_target = get_node(target_path)
+	if _target:
+		global_position = _target.global_position
 
 
 func _physics_process(delta: float) -> void:
-	if not target:
+	if not _target:
 		return
 
 	var target_vel := Vector3.ZERO
-	if target is RigidBody3D:
-		target_vel = target.linear_velocity
+	if _target is RigidBody3D:
+		target_vel = _target.linear_velocity
 	_current_velocity = _current_velocity.lerp(target_vel, delta * 3.0)
 
 	var speed_kmh := _current_velocity.length() * 3.6
@@ -44,7 +47,7 @@ func _physics_process(delta: float) -> void:
 	# Look-ahead: offset target position by velocity direction
 	var look_ahead := _current_velocity * look_ahead_strength
 	look_ahead.y = 0.0
-	var target_pos := target.global_position + look_ahead
+	var target_pos := _target.global_position + look_ahead
 
 	# Smooth position follow
 	global_position = global_position.lerp(target_pos, delta * follow_speed)
@@ -59,5 +62,5 @@ func _physics_process(delta: float) -> void:
 			rotation.y = lerp_angle(current_angle, target_angle + PI, delta * rotation_speed)
 	else:
 		# When stopped, follow vehicle rotation
-		var target_angle := target.rotation.y
+		var target_angle := _target.rotation.y
 		rotation.y = lerp_angle(rotation.y, target_angle + PI, delta * rotation_speed)
