3377167afce8bbb64b24b086f42f35e87ae2f5d4
Cristian Magherusan-Stanciu <cristi.magherusan@gmail.com>
2026-03-02 11:32:12 +0100

    Add police vehicle scene with light bar and siren
    - Inherit base_vehicle.tscn with white/blue color scheme
    - PoliceLightBar: alternating red/blue OmniLight3D flashes
    - PoliceSiren node ready for pursuit activation
    - Collision layer 7 (Police) = bit 64

---
+++ scenes/vehicles/police_light_bar.gd
@@ -0,0 +1,48 @@
+extends Node3D
+## Alternating red/blue police light bar with OmniLight3D nodes.
+
+const FLASH_INTERVAL := 0.15
+
+var lights_active := false
+
+var _timer := 0.0
+var _red_on := true
+var _red_light: OmniLight3D = null
+var _blue_light: OmniLight3D = null
+
+
+func _ready() -> void:
+	_red_light = OmniLight3D.new()
+	_red_light.light_color = Color(1.0, 0.1, 0.1)
+	_red_light.light_energy = 3.0
+	_red_light.omni_range = 15.0
+	_red_light.position = Vector3(-0.4, 0.0, 0.0)
+	add_child(_red_light)
+
+	_blue_light = OmniLight3D.new()
+	_blue_light.light_color = Color(0.1, 0.2, 1.0)
+	_blue_light.light_energy = 3.0
+	_blue_light.omni_range = 15.0
+	_blue_light.position = Vector3(0.4, 0.0, 0.0)
+	add_child(_blue_light)
+
+	_set_lights(false, false)
+
+
+func _process(delta: float) -> void:
+	if not lights_active:
+		_set_lights(false, false)
+		return
+
+	_timer += delta
+	if _timer >= FLASH_INTERVAL:
+		_timer = 0.0
+		_red_on = not _red_on
+	_set_lights(_red_on, not _red_on)
+
+
+func _set_lights(red_on: bool, blue_on: bool) -> void:
+	if _red_light:
+		_red_light.visible = red_on
+	if _blue_light:
+		_blue_light.visible = blue_on
